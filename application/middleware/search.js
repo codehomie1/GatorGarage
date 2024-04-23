b8b7362cf1ca0159a47fc0c09aa7046078f69c0d
Utku Tarhan <mtarhan@apple.com>
2024-04-23 01:09:18 -0700

    Revert "Merge branch 'main' of github.com:CSC-648-SFSU/csc648-sp24-03-Team06 into staging"
    This reverts commit baf94aac3897b3c79f202dd0eee3bca14e10afad, reversing
    changes made to 7bae30caa7dfd063aa5baa8e2cb82ee53854c53c.

--- application/middleware/search.js
+++ application/middleware/search.js
@@ -5,11 +5,7 @@ module.exports = {
     try {
       // Extracting search query and category from request parameters
       const searchQuery = req.query.query; // Passed through query string
-<<<<<<< HEAD
       let selectedCategory = req.query.category || ''; // Selected category through query string or empty string
-=======
-      let selectedCategory = req.query.category; // Selected category through query string
->>>>>>> 8ed4f1b8e01ff78223fb3388ffe846caa72d14f3
 
       if (selectedCategory === '' || selectedCategory.toLowerCase() === 'all') {
         selectedCategory = null; // Set to null for clarity in handling the no-category case
@@ -21,7 +17,6 @@ module.exports = {
       if (selectedCategory) {
         // If a category is selected, include it in the search criteria
         query = `
-<<<<<<< HEAD
         SELECT itemPost.*, categories.categoryName AS category, users.username AS sellerName, users.email AS sellerEmail
         FROM csc648_team6DB.itemPost
         INNER JOIN csc648_team6DB.categories ON itemPost.categoryId = categories.categoryId
@@ -29,14 +24,6 @@ module.exports = {
         WHERE (itemPost.itemName LIKE ? OR itemPost.itemDescription LIKE ?)
         AND itemPost.isSold = 0
         AND categories.categoryName = ?`;
-=======
-          SELECT itemPost.*, categories.categoryName AS category
-          FROM csc648_team6DB.itemPost
-          INNER JOIN csc648_team6DB.categories ON itemPost.categoryId = categories.categoryId
-          WHERE (itemPost.itemName LIKE ? OR itemPost.itemDescription LIKE ?)
-            AND itemPost.isSold = 0
-            AND categories.categoryName = ?`;
->>>>>>> 8ed4f1b8e01ff78223fb3388ffe846caa72d14f3
 
         // Prepare values for parameterized query to prevent SQL injection
         values = [`%${searchQuery}%`, `%${searchQuery}%`, selectedCategory];
@@ -44,20 +31,12 @@ module.exports = {
       } else {
         // General search without specific category selection (all categories)
         query = `
-<<<<<<< HEAD
         SELECT itemPost.*, categories.categoryName AS category, users.username AS sellerName, users.email AS sellerEmail
         FROM csc648_team6DB.itemPost
         INNER JOIN csc648_team6DB.categories ON itemPost.categoryId = categories.categoryId
         INNER JOIN csc648_team6DB.users ON itemPost.userId = users.userId
         WHERE (itemPost.itemName LIKE ? OR itemPost.itemDescription LIKE ?)
         AND itemPost.isSold = 0`;
-=======
-          SELECT itemPost.*, categories.categoryName AS category
-          FROM csc648_team6DB.itemPost
-          INNER JOIN csc648_team6DB.categories ON itemPost.categoryId = categories.categoryId
-          WHERE (itemPost.itemName LIKE ? OR itemPost.itemDescription LIKE ?)
-            AND itemPost.isSold = 0`;
->>>>>>> 8ed4f1b8e01ff78223fb3388ffe846caa72d14f3
 
         values = [`%${searchQuery}%`, `%${searchQuery}%`];
       }
@@ -68,7 +47,6 @@ module.exports = {
       // Handling empty result cases by returning random unsold items
       if (result.length === 0) {
         res.locals.isEmptyResult = true;
-<<<<<<< HEAD
         query = `SELECT itemPost.*, categories.categoryName AS category, users.username AS sellerName, users.email AS sellerEmail
         FROM csc648_team6DB.itemPost
         INNER JOIN csc648_team6DB.categories ON itemPost.categoryId = categories.categoryId
@@ -77,14 +55,6 @@ module.exports = {
         ORDER BY RAND()
         LIMIT 10`;
         
-=======
-        query = `SELECT itemPost.*, categories.categoryName AS category
-                 FROM csc648_team6DB.itemPost
-                 INNER JOIN csc648_team6DB.categories ON itemPost.categoryId = categories.categoryId
-                 WHERE itemPost.isSold = 0
-                 ORDER BY RAND()
-                 LIMIT 10`;
->>>>>>> 8ed4f1b8e01ff78223fb3388ffe846caa72d14f3
         const [emptyQueryResult] = await db.query(query);
         res.locals.items = emptyQueryResult;
         next();
--- application/public/js/__test__/getCategories.test.js
+++ application/public/js/__test__/getCategories.test.js
@@ -1,5 +1,4 @@
 
-
 const {getCategories} = require('../../../middleware/getCategories.js');
 const {getCategoriesWithPictures} = require('../../../middleware/getCategories.js');
 
