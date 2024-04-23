1703fc9386d6c902b8ae6c7323009a04d4837dd3
cesar-programs <84650530+cesar-programs@users.noreply.github.com>
2024-04-23 00:58:05 -0700

    navbar persistency polish -> develop (#111)
    * Add padding to container heading to keep it from edge of browser
    
    * Update result display title to account for category filter searches
    
    * Fix category persistence in navbar search
    
    ---------
    
    Co-authored-by: Cesar A. Herrera <Cesar@Lauren.local>

--- application/public/js/searchAndCategory.js
+++ application/public/js/searchAndCategory.js
@@ -8,7 +8,12 @@ document.addEventListener('DOMContentLoaded', function(e) {
 
   // persists query and search
   document.querySelector('input[name="query"]').value = query || '';
-  document.querySelector('#category-btn').innerText = category || "All";
+    const categoryButton = document.querySelector('#category-btn');
+    categoryButton.innerText = category || "All";
+
+    // Set the hidden input value to match the category button for consistent data handling
+    document.querySelector('input[name="category"]').value = categoryButton.innerText;
+
 
     // updates the dropdown to have the  selected category when an option is clicked.
     // sends input category param to backend.
